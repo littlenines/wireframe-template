cd8e3cccd9adf6e63f5e0e0a70ecf0ac5050c1ef
littlenines <nemanja584@hotmail.com>
2024-06-04 23:29:15 +0200

    add footer

--- src/components/Layout/Footer/index.jsx
+++ src/components/Layout/Footer/index.jsx
@@ -1,7 +1,17 @@
+import style from './Footer.module.scss'
+
 const Footer = () => {
   return (
-    <footer>
-        footer
+    <footer className={style.footer}>
+      <div className={style.socials}>
+        <img src="icons/twitter.svg" width={24} height={24} alt="twitter" />
+        <img src="icons/facebook2.svg" width={24} height={24} alt="facebook" />
+        <img src="icons/instagram.svg" width={24} height={24} alt="instagram" />
+      </div>
+      <div className={style.copyright}>
+        <img src="icons/copyright.svg" width={24} height={24} alt="copyright" />
+        <span className="anchor_typography">Copyright, 2021</span>
+      </div>
     </footer>
   )
 }
