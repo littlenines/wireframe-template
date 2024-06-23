647d14a4f022ff15dedf4ab62aeb140dc041f028
littlenines <nemanja584@hotmail.com>
2024-06-23 22:34:40 +0200

    add props for info component

--- src/components/Info/index.jsx
+++ src/components/Info/index.jsx
@@ -1,30 +1,33 @@
-// TODO: add props
-import style from './Info.module.scss'
+import style from './Info.module.scss';
 import PropTypes from 'prop-types';
 
 const Info = (props) => {
-  const { title='London, UK' } = props
+  const defaultContent = [
+    { id: '09ca02c0', icon: 'icons/location.svg', title: 'Address' },
+    { id: '95992e5c', icon: 'icons/email.svg', title: 'E-mail Address' },
+    { id: 'dd87b95a', icon: 'icons/phone.svg', title: 'Phone Number' },
+  ];
+  const { title = 'London, UK', content = defaultContent } = props;
   return (
     <div className={style.info} {...props}>
       {title && <h3 className={style.info_title}>{title}</h3>}
-      <div className={style.info_icon}>
-        <img src="icons/location.svg" width={24} height={24} alt="" />
-        <span className="section_typography">Address</span>
-      </div>
-      <div className={style.info_icon}>
-        <img src="icons/email.svg" width={24} height={24} alt="" />
-        <span className="section_typography">E-mail Address</span>
-      </div>
-      <div className={style.info_icon}>
-        <img src="icons/phone.svg" width={24} height={24} alt="" />
-        <span className="section_typography">Phone Number</span>
-      </div>
+      {content.map(info => 
+        <div key={info.id} className={style.info_icon}>
+          <img src={info.icon} width={24} height={24} alt={info.title} />
+          <span className="section_typography">{info.title}</span>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
 Info.propTypes = {
   title: PropTypes.string,
+  content: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string,
+    icon: PropTypes.string,
+    title: PropTypes.string,
+  })),
 };
 
 export default Info;
