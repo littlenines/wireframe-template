7d3c67dbc523a4315693265a0639bf926c73fd7e
littlenines <nemanja584@hotmail.com>
2024-06-23 22:14:21 +0200

    fix hero css naming

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -12,11 +12,11 @@ const Hero = (props) => {
 `
   return (
     <section className={style.hero} css={hero_style} {...props}>
-      <div className={`container ${style.content} max-content`}>
-        <h1 className={`${style.text_content} heading_typography`}>
+      <div className={`container ${style.hero_content} max-content`}>
+        <h1 className={`${style.hero_content_text} heading_typography`}>
           {title}
         </h1>
-        <p className={`${style.text_content} section_typography barely_visible`}>
+        <p className={`${style.hero_content_text} section_typography barely_visible`}>
           {description}
         </p>
         <Button size="large" className='anchor_typography' />
