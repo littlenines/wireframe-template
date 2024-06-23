8f61be7e60861ec2f46c8db44b7a0de18dc1d8ce
littlenines <nemanja584@hotmail.com>
2024-06-23 22:37:04 +0200

    fix footer css naming

--- src/components/Layout/Footer/index.jsx
+++ src/components/Layout/Footer/index.jsx
@@ -4,12 +4,12 @@ const Footer = () => {
   return (
     <footer className={style.footer}>
       <div className='max-content'>
-        <div className={style.socials}>
+        <div className={style.footer_socials}>
           <img src="icons/twitter.svg" width={24} height={24} loading='lazy' alt="twitter" />
           <img src="icons/facebook2.svg" width={24} height={24} loading='lazy' alt="facebook" />
           <img src="icons/instagram.svg" width={24} height={24} loading='lazy' alt="instagram" />
         </div>
-        <div className={style.copyright}>
+        <div className={style.footer_copyright}>
           <img src="icons/copyright.svg" width={24} height={24} loading='lazy' alt="copyright" />
           <span className="anchor_typography">Copyright, 2021</span>
         </div>
